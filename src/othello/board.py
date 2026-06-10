6c5a6c3e9dbb0dd8dac87f8c537d35aa67b06a86
adamashton67 <adamashton67@proton.me>
2026-06-10 14:57:41 +0100

    Add directional piece capture logic

--- src/othello/board.py
+++ src/othello/board.py
@@ -3,6 +3,17 @@ BLACK = "B"
 WHITE = "W"
 BOARD_SIZE = 8
 
+DIRECTIONS = [
+    (-1, 0),   # up
+    (1, 0),    # down
+    (0, -1),   # left
+    (0, 1),    # right
+    (-1, -1),  # up left
+    (-1, 1),   # up right
+    (1, -1),   # down left
+    (1, 1),    # down right
+]
+
 def create_starting_board():
     board = [[EMPTY for i in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]
 
@@ -21,6 +32,41 @@ def print_board(board):
 def is_empty_square(board, row, column):
     return board[row][column] == EMPTY
 
+def is_on_board(row, column):
+    return (
+        0 <= row < BOARD_SIZE
+        and
+        0 <= column < BOARD_SIZE
+    )
+
+def get_opponent(player):
+    if player == BLACK:
+        return WHITE
+    else:
+        return BLACK
+    
+def find_flips_in_direction(board, row, column, player, row_change, column_change):
+    opponent = get_opponent(player)
+
+    row += row_change
+    column += column_change
+
+    flips = []
+    while is_on_board(row, column):
+        current_square = board[row][column]
+        if current_square == opponent:
+            flips.append((row, column))
+            row += row_change
+            column += column_change
+            continue 
+        elif current_square == player:
+            return flips
+        elif current_square == EMPTY:
+            return []
+        else:
+            return []
+
+
 if __name__ == "__main__":
     board = create_starting_board()
 
@@ -29,3 +75,8 @@ if __name__ == "__main__":
     print()
     print(is_empty_square(board, 0, 0))
     print(is_empty_square(board, 3, 3))
+    print()
+    print(is_on_board(0, 0))
+    print(is_on_board(7, 7))
+    print(is_on_board(8, 0))
+    print(is_on_board(-1, 3))
